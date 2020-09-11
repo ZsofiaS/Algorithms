e57f6dc314e4412575d99219ed9665a730de3c13
Zsofia Szonyi <szonyi.zsofia@gmail.com>
2020-09-11 14:24:55 +0100

    Add breadthfirst function

--- breadthFirstSearch/spec/breadthFirstSearchSpec.js
+++ breadthFirstSearch/spec/breadthFirstSearchSpec.js
@@ -1,1 +1,36 @@
 'use strict';
+
+describe('Search', () => {
+  let search = new Search();
+  describe('#breadthFirst', () => {
+    it('returns false if graph is empty', () => {
+      let graph1 = {
+        'you': []
+      };
+      expect(search.breadthFirst(graph1, 'you')).toEqual(false);
+    });
+    it('returns false with an example graph where starting point has 1 friend, and the name does not contain cat', () => {
+      let graph2 = {
+        'you': ['a'],
+        'a': []
+      };
+      expect(search.breadthFirst(graph2, 'you')).toEqual(false);
+    });
+    it('returns true with an example graph where starting point has 1 friend, and the name does contain cat', () => {
+      let graph3 = {
+        'you': ['a cat'],
+        'a cat': []
+      };
+      expect(search.breadthFirst(graph3, 'you')).toEqual('a cat');
+    });
+    it('returns true with an example graph where starting point has 3 friend, and one of the names does contain cat', () => {
+      let graph4 = {
+        'you': ['a', 'b'],
+        'a': [],
+        'b': ['c'],
+        'c': ['d cat']
+      };
+      expect(search.breadthFirst(graph4, 'you')).toEqual('d cat');
+    });
+  })
+})
--- breadthFirstSearch/src/breadthFirstSearch.js
+++ breadthFirstSearch/src/breadthFirstSearch.js
@@ -1,1 +1,23 @@
 'use strict';
+
+class Search {
+  breadthFirst(graph, start) {
+    let queue = [];
+    queue.push(...graph[start]);
+    let searched = [];
+    while (queue.length > 0) {
+      let person = queue[0];
+      if (!searched.includes(person)) {
+        if (person.includes('cat')) {
+          queue.shift();
+          return person;
+        } else {
+          queue.shift();
+          queue.push(...graph[person]);
+          searched.push(person);
+        }
+      }
+    }
+    return false;
+  }
+}
